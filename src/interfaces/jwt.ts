6ef9e00636f5a38c9b18689f07b7935d35d3c698
Aditya Garg <mynameisaditya824@gmail.com>
2024-10-02 00:25:40 +0530

    added user and session jwt props

---
+++ src/interfaces/jwt.ts
@@ -0,0 +1,10 @@
+import { JwtPayload } from "jsonwebtoken"
+
+export interface SessionJwtPayload extends JwtPayload{
+    sessionID:string
+}
+
+export interface UserJwtPayload extends JwtPayload{
+    userId:string
+    name:string
+}
