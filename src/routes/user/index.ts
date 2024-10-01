8a4ca09eb8c2253b636a7bef8d41421692962b86
Aditya Garg <mynameisaditya824@gmail.com>
2024-10-02 00:30:15 +0530

    added list of user routes and completed login and logout routes

---
+++ src/routes/user/index.ts
@@ -0,0 +1,15 @@
+import express, { Router } from "express"
+import { isValidSession } from "../../middlewares/session"
+import { Login, Logout } from "../../controllers/user"
+const router:Router=express.Router()
+
+router.post("/register-user")
+router.post("/login",Login)
+router.post("/logout",isValidSession,Logout)
+router.put("/change-password")
+router.put("/forgot-password")
+router.get("/send-otp")
+router.post("/verify-otp")
+router.delete("/remove-user")
+
+export default router
