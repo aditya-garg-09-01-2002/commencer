d7da0458b10d164769cb1e935a94183b3fe94067
Aditya Garg <mynameisaditya824@gmail.com>
2024-10-04 19:47:15 +0530

    segregated responsibility of register controller to start verification process of user
    Register controller will check if a user exists or not, if not then initiates the verification process for the user, else generates appropriate error message

--- src/controllers/user/register.ts
+++ src/controllers/user/register.ts
@@ -1,35 +1,43 @@
-import bcrypt from "bcrypt"
 import { Request, Response } from "express";
 import { database } from "../../config/database";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { uuid } from "uuidv4";
+import { hash } from "bcrypt";
+import { sendOtp } from "../../utils/communication";
 
 export default async function registerUser(req:Request,res:Response){
     try{
-        const {userId,password,name} = req.body;
-        const passwordHash = await bcrypt.hash(password,12)
-        const newUser = await database.user.create({data:{
-            userId:userId,
-            passwordHash:passwordHash,
-            profile:{
-                create:{
-                    name:name
+        const {userId,name} = req.body;
+        const idType = req.params.idType
+        const user = await database.user.findUnique({where:{userId:userId}})
+        if(!user){
+            const otp = uuid()
+            const otpHash = await hash(otp,12)
+            const newUser = await database.user.create({data:{
+                userId:userId, 
+                profile:{
+                    create:{
+                        name:name 
+                    }
+                },
+                otp:{
+                    create:{
+                        otpHash:otpHash
+                    }
                 }
-            }
-        }})
-        res.status(200).json({fetched:true,message:"User successfully registered."})
-    }catch(error){
-        if(error instanceof PrismaClientKnownRequestError){
-            if(error.code == "P2002"){
-                res.status(401).json({fetched:false,message:"User already exists!!!\nKindly move to login."})
-            }
-            else{
-                console.log(error)
-                res.status(400).json({fetched:false,message:"Error registering user.\nKindly Report."})
-            }
+            }})
+            res.status(200).json({fetched:true,message:"User successfully registered.\nKindly verify using mail."})
+            sendOtp(userId,otp,idType)
         }
         else{
-            console.log(error)
-            res.status(500).json({fetched:false,message:"Internal Server Error"})
+            if(!user.passwordHash){
+                res.status(401).json({fetched:false,message:"User registered but password never set!!!\nKindly set password first."})
+            }
+            else{
+                res.status(403).json({fetched:false,message:"User already exits!!!\nMove to Login."})
+            }
         }
+    }catch(error){
+        console.log(error)
+        res.status(500).json({fetched:false,message:"Internal Server Error"})
     }
 }
