d1a2670d0ac3ef00e917a52166316338764640e8
Aditya Garg <mynameisaditya824@gmail.com>
2024-10-04 21:17:11 +0530

    Added getUserDetails middlewares
    getUserDetails are responsible for fetching user details and add them to express request.
    
    middlewares added - getUserDetailsFromAPIBody and getUserDetailsFromJwt to extract userId from api request body and jwt token respectively.
    fetch the user details from database for respective userId and add them to express request.

---
+++ src/middlewares/auth/getUserDetails.ts
@@ -0,0 +1,60 @@
+import { NextFunction } from "express";
+import { UserRequest, UserResponse } from "../../interfaces/express";
+import jwt from "jsonwebtoken"
+import { UserJwt } from "../../config/jwt";
+import { UserJwtPayload } from "../../interfaces/jwt";
+import { isEmptyStr } from "../../utils/dataFormat";
+import { database } from "../../config/database";
+
+async function getUserDetails(req:UserRequest,res:UserResponse,next:NextFunction,userId:string|undefined|null){
+    try{
+        if(isEmptyStr(userId)){
+            res.status(404).json({fetched:false,message:"User ID could not be fetched."})
+        }
+        else{
+            req.userID = userId as string
+            const user = await database.user.findUnique({where:{userId:req.userID}})
+            if(!user){
+                res.status(500).json({fetched:false,message:"User not registered!!!\nKindly consider registering user."})
+            }
+            else{
+                req.user = user;
+                next()
+            }
+        }
+    }catch(error){
+        console.log(error)
+        res.status(500).json({message:"Internal Server Error"})
+    }
+}
+
+export async function getUserDetailsFromJwt(req:UserRequest,res:UserResponse,next:NextFunction){
+    try{
+        const userJwtToken:string|undefined = req.params.userJwtToken
+        if(userJwtToken===undefined){
+            res.status(404).json({message:"User data not found!!!\nKindly Login Again"})
+        }
+        else{
+            const jwtTokenData = jwt.verify(userJwtToken,UserJwt.key) as UserJwtPayload
+            if(isEmptyStr(jwtTokenData.userId)){
+                res.status(401).json({fetched:false,message:"User Details not found!!!\nKindly Login again"})
+            }
+            else{
+                getUserDetails(req,res,next,jwtTokenData.userId)
+            }
+        }
+    }catch(error){
+        console.log(error)
+        res.status(500).json({message:"Internal Server Error"})
+    }
+}
+
+export async function getUserDetailsFromAPIBody(req:UserRequest,res:UserResponse,next:NextFunction){
+    try{
+        const userId:string|undefined = req.body.userId
+        await getUserDetails(req,res,next,userId)
+    }catch(error){
+        console.log(error)
+        res.status(500).json({message:"Internal Server Error"})
+    }
+}
