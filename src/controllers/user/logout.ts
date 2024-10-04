5a8e75d17b4677d9ddf41a7ff81780f61d996d95
aditya-garg-09-01-2002 <mynameisaditya824@gmail.com>
2024-10-05 00:11:25 +0530

    added 'fetched' object attribute to logout controller missing attribute leads to inappropriate displays of the error messsage determining flag.
    Issue Reference: #<issue-number>

--- src/controllers/user/logout.ts
+++ src/controllers/user/logout.ts
@@ -6,6 +6,6 @@ export default async function Logout(req:Request,res:Response){
         res.status(200).json({fetched:true,message:"Successfully Logged Out!!!"})
     }catch(error){
         console.log(error)
-        res.status(500).json({message:"Internal Server Error"})
+        res.status(500).json({message:"Internal Server Error",fetched:false})
     }
 }
