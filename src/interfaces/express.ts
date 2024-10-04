13d4f5e24f51b0061eae3ddadcc38f918b9392e8
aditya-garg-09-01-2002 <mynameisaditya824@gmail.com>
2024-10-04 23:54:01 +0530

    added user data to UserRequest interface for express request
    to store and pass the user data along the express request.
    
    added user data with UserProps to the UserRequest interface
    
    Issue Reference: #<issue-number>

--- src/interfaces/express.ts
+++ src/interfaces/express.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
+import { UserProps } from "./user";
 
 export interface UserRequest extends Request{
     userID ?: string
     name ?: string
+    user ?: UserProps | null
 }
 
 export interface UserResponse extends Response{
