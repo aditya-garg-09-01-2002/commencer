0de3b6a9ef86ec4c2e6bbc0d2508055a158e9c88
Arjun Garg <arjungarg@Arjuns-Laptop.local>
2024-10-04 23:19:30 +0530

    Added export map file for different auth middlewares
    Provides abstraction over various auth middlewares been allowed to be used
    
    Added a simple file to import all default or modular exports from the common folder and export them.

---
+++ src/middlewares/auth/index.ts
@@ -0,0 +1,10 @@
+import verifyOtp from "./verifyOtp"
+import verifyPassword from "./verifyPassword"
+import { getUserDetailsFromAPIBody,getUserDetailsFromJwt } from "./getUserDetails"
+
+export {
+    verifyOtp,
+    verifyPassword,
+    getUserDetailsFromJwt,
+    getUserDetailsFromAPIBody
+}
