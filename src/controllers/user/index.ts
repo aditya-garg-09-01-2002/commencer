6fa2d31452f7d1b20e0deb22be79cb3e5101af65
aditya-garg-09-01-2002 <mynameisaditya824@gmail.com>
2024-10-05 00:18:02 +0530

    added more controllers export map for user controllers more controllers were created, they were meant to be exported for further use.
    imported default/module functions from common folder and export them in an abstract format.
    
    Issue Reference: #<issue-number>

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -1,7 +1,20 @@
-import Login from "./login";
-import Logout from "./logout";
+import login from "./login";
+import logout from "./logout";
+import registerUser from "./register"
+import changePassword from "./changePassword";
+import forgotPassword from "./forgotPassword";
+import resendOtp from "./resendOtp";
+import { resetPassword,setPassword } from "./updatePassword";
+import removeUser from "./removeUser";
 
 export {
-    Login,
-    Logout
+    login,
+    logout,
+    registerUser,
+    changePassword,
+    forgotPassword,
+    resendOtp,
+    resetPassword,
+    setPassword,
+    removeUser
 }
