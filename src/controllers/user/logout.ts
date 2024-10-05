56c38d2439d023838153623f43da90a5c8796ceb
aditya-garg-09-01-2002 <mynameisaditya824@gmail.com>
2024-10-05 19:08:32 +0530

    updated logout controller
    logout clears cookie based on cookie name from cookie config file.
    change static name of session cookie to dynamic as fetched from cookie config file
    
    Issue Reference: #<issue-number>

--- src/controllers/user/logout.ts
+++ src/controllers/user/logout.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express";
+import { SessionCookie } from "../../config/cookie";
 
 export default async function Logout(req:Request,res:Response){
     try{
-        res.clearCookie("Session-ID")
+        res.clearCookie(SessionCookie.name)
         res.status(200).json({fetched:true,message:"Successfully Logged Out!!!"})
     }catch(error){
         console.log(error)
