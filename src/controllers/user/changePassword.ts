b04a743f830a7fd6382630d9755e4831fe19b36a
aditya-garg-09-01-2002 <mynameisaditya824@gmail.com>
2024-10-05 14:32:27 +0530

    removed unused dependency
    bcrypt compare is unused in changePassword controller and thus removed.
    
    Issue Reference: #<issue-number>

--- src/controllers/user/changePassword.ts
+++ src/controllers/user/changePassword.ts
@@ -1,4 +1,4 @@
-import { compare, hash } from "bcrypt";
+import { hash } from "bcrypt";
 import { database } from "../../config/database";
 import { UserRequest, UserResponse } from "../../interfaces/express";
 
