55e8c1b78c57882d4a8157ed56fcaf88b40ceb95
aditya-garg-09-01-2002 <mynameisaditya824@gmail.com>
2024-10-06 19:55:31 +0530

    updated communication utilities
    integration of mailing utility complete
    
    utilising tranporter created by nodemailer to send mails by provided credentails.
    integration of sendMail function to be used in mailOtp function to send the mail containing otp.
    
    Issue Reference: #<issue-number>

--- src/utils/communication.ts
+++ src/utils/communication.ts
@@ -1,16 +1,46 @@
-export function sendOtp(userId:string,otpHash:string,idType:string){
+import { APP_NAME } from "../config/app"
+import { getTransporter } from "../config/mail"
+import { isEmptyStr } from "./dataFormat"
+
+export async function sendOtp(userId:string,otpHash:string,idType:string){
     if(idType === "mobile"){
-        messageOtp(userId,otpHash)
+        await messageOtp(userId,otpHash)
     }
     else if(idType === "email"){
-        mailOtp(userId,otpHash)
+        await mailOtp(userId,otpHash)
     }
 }
 
-function messageOtp(userId:string,otpHash:string){
-
+async function messageOtp(userId:string,otpHash:string){
+    console.log("Currently sending otp on mobile is not supported")
+    console.log("Please consider setting up mailing otp as an alternative")
+    console.log("Please help our github repo if you know how to do this stuff")
 }
 
-function mailOtp(userId:string,otpHash:string){
+async function mailOtp(userId:string,otpHash:string){
+    await sendMail([userId],isEmptyStr(APP_NAME)?`Verification code for ${APP_NAME}`:"Verification Code",`<div>${otpHash}</div>`)    
+}
 
+export async function sendMail(recipients:string[],subject:string,html:string){
+  try {
+    const results = await Promise.all(
+      recipients.map(async (recipient) => {
+        try {
+          const transporter=getTransporter()
+          await transporter.sendMail({
+            to: recipient,
+            subject: subject,
+            html:html
+          });
+          return { recipient: recipient, sent: true };
+        } catch (error:any) {
+          console.log(error)
+          return { recipient: recipient, sent: false, error: error.message };
+        }
+      })
+    );
+    return results;
+  } catch (error) {
+    throw new Error("Failed to send emails");
+  }
 }
