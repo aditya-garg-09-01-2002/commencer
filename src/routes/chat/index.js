be7caf32475d25a90e39ae5fec6ee8a09c6fc905
ajmeraaditya02 <ajmeraaditya05@gmail.com>
2024-10-14 00:46:49 +0530

    added create group route to the chat feature
    to facilitate developers to use create-group route
    
    a new chat group route was created
    
    Issue Reference: #<issue-number>

--- src/routes/chat/index.js
+++ src/routes/chat/index.js
@@ -5,14 +5,4 @@ import {isValidSession} from "../../middlewares/session/isValidSession"
 
 router.post("/create-group", isValidSession);
 
-// router.post("/register-user",registerUser)
-// router.post("/login",getUserDetailsFromAPIBody,verifyPassword,login)
-// router.post("/logout",isValidSession,logout)
-// router.get("/forgot-password",forgotPassword)
-// router.get("/change-password",isValidSession,getUserDetailsFromJwt,verifyPassword,changePassword)
-// router.put("/resend-otp",resendOtp)
-// router.put("/set-password",verifyOtp,setPassword)
-// router.put("/reset-password",verifyOtp,resetPassword)
-// router.delete("/remove-user",isValidSession,getUserDetailsFromJwt,verifyPassword,removeUser)
-
 export default router;
