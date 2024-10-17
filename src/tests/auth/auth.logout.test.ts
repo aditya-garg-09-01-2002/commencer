776e587a966ead52a65a928324d6e0a2ec098a3b
aditya-garg-09-01-2002 <mynameisaditya824@gmail.com>
2024-10-17 20:09:48 +0530

    Updated tests created for authentication
    to due to previous code structure, difference b/w test suite and test case was not correctly explained.
    
    - test cases' set for each API is now a test suite
    - in a test suite, there lie multiple test cases which cover scenarios for the API
    
    Issue Reference: #<issue-number>

--- src/tests/auth/auth.logout.test.ts
+++ src/tests/auth/auth.logout.test.ts
@@ -9,87 +9,88 @@ import { sign } from "cookie-signature"
 
 describe("logout api tests", ()=>{
 
-    describe("valid session jwt is provided and session cookie is present",()=>{
+    test("valid session jwt is provided and session cookie is present",async()=>{
         let response:Response
-        beforeAll(async ()=>{
-            const sessionId = v4()
-            response = await request(app)
+        const sessionId = v4()
+        response = await request(app)
+            .post("/auth/logout")
+            .set(SessionJwt.name,generateSessionJwt(sessionId,SessionJwt.key))
+            .set('Cookie',`${SessionCookie.name}=s:${sign(sessionId,SessionCookie.key)}`)
+            
+        // should respond with a status code 200
+        expect(response.status).toBe(200)
+
+        //should respond with a json object containing message
+        expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
+        expect(response.body).toHaveProperty("fetched")
+        expect(response.body.fetched).toBe(true)
+        expect(response.body).toHaveProperty("message")
+        expect(response.body.message).toBeDefined()
+    })
+
+    const sessionId = v4()
+    const testCasesMissingSessionCredentials = [
+        {
+            Cookie:`${SessionCookie.name}=s:${sign(sessionId,v4())}`,
+        },
+        {
+            [SessionJwt.name]:generateSessionJwt(sessionId,SessionJwt.key)
+        },
+    ]
+    testCasesMissingSessionCredentials.forEach((testCase:{
+        [x:string]:string
+    })=>{
+        test("either session cookie or session jwt is absent, due to expiry or previous logout",async()=>{
+            let response:Response
+            let req = request(app)
                 .post("/auth/logout")
-                .set(SessionJwt.name,generateSessionJwt(sessionId,SessionJwt.key))
-                .set('Cookie',`${SessionCookie.name}=s:${sign(sessionId,SessionCookie.key)}`)
-        })
-        test("should respond with a status 200 code", async ()=>{
-            expect(response.status).toBe(200)
-        })
-        test("should respond with a json object containing message", async ()=>{
+
+            Object.keys(testCase).forEach(key=>{
+                req.set(key,testCase[key])
+            })
+    
+            response = await req
+
+            // should respond with a status 401 code
+            expect(response.status).toBe(401)
+
+            // should respond with a json object containing message
             expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
             expect(response.body).toHaveProperty("fetched")
-            expect(response.body.fetched).toBe(true)
+            expect(response.body.fetched).toBe(false)
             expect(response.body).toHaveProperty("message")
             expect(response.body.message).toBeDefined()
         })
     })
 
-    describe("either session cookie or session jwt is absent, due to expiry or previous logout",()=>{
-        let response:Response
-        const sessionId = v4()
-        const testCases = [
-            {
-                Cookie:`${SessionCookie.name}=s:${sign(sessionId,v4())}`,
-            },
-            {
-                [SessionJwt.name]:generateSessionJwt(sessionId,SessionJwt.key)
-            },
-        ]
-        testCases.forEach(testCase=>{
-            let req = request(app)
-                .post("/auth/logout")
-            Object.keys(testCase).forEach(key=>{
-                req.set(key,testCase[key])
-            })
-            beforeAll(async ()=>{
-                response = await req
-            })
-            test("should respond with a status 401 code", async ()=>{
-                expect(response.status).toBe(401)
-            })
-            test("should respond with a json object containing message", async ()=>{
-                expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
-                expect(response.body).toHaveProperty("fetched")
-                expect(response.body.fetched).toBe(false)
-                expect(response.body).toHaveProperty("message")
-                expect(response.body.message).toBeDefined()
-            })
-        })
-    })
+    const testCasesInvalidSession = [
+        {
+            Cookie:`${SessionCookie.name}=s:${sign(v4(),SessionCookie.key)}`,
+            [SessionJwt.name]:generateSessionJwt(v4(),SessionJwt.key)
+        },
+    ]
 
-    describe("mismatch of session ID between cookie and jwt",()=>{
-        let response:Response
-        const testCases = [
-            {
-                Cookie:`${SessionCookie.name}=s:${sign(v4(),SessionCookie.key)}`,
-                [SessionJwt.name]:generateSessionJwt(v4(),SessionJwt.key)
-            },
-        ]
-        testCases.forEach(testCase=>{
+    testCasesInvalidSession.forEach((testCase:{
+        [x:string]:string
+    })=>{
+        test("mismatch of session ID between cookie and jwt",async ()=>{
+            let response:Response
             let req = request(app)
                 .post("/auth/logout")
             Object.keys(testCase).forEach(key=>{
                 req.set(key,testCase[key])
             })
-            beforeAll(async ()=>{
-                response = await req
-            })
-            test("should respond with a status 403 code", async ()=>{
-                expect(response.status).toBe(403)
-            })
-            test("should respond with a json object containing message", async ()=>{
-                expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
-                expect(response.body).toHaveProperty("fetched")
-                expect(response.body.fetched).toBe(false)
-                expect(response.body).toHaveProperty("message")
-                expect(response.body.message).toBeDefined()
-            })
+            response = await req
+
+            // should respond with a status 403 code
+            expect(response.status).toBe(403)
+
+            // should respond with a json object containing message
+            expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
+            expect(response.body).toHaveProperty("fetched")
+            expect(response.body.fetched).toBe(false)
+            expect(response.body).toHaveProperty("message")
+            expect(response.body.message).toBeDefined()
         })
     })
 })
